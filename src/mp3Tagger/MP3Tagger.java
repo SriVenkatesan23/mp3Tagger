af1cc81d42f7707bf46610dfc95f50124d0bb264
Srihari Venkatesan <sri.venkatesan23@gmail.com>
2016-08-17 23:21:06 -0400

    GUI updated

--- src/mp3Tagger/MP3Tagger.java
+++ src/mp3Tagger/MP3Tagger.java
@@ -1,11 +1,8 @@
 package mp3Tagger;
-import java.io.File;
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
+import java.awt.event.*;
 import java.awt.*;
 import java.io.*;
-import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.*;
 import com.mpatric.mp3agic.*;
@@ -25,7 +22,8 @@ public class MP3Tagger extends JFrame {
 		//INPUT TEXT AREA
 		textbox.setBackground(Color.darkGray);
 		textbox.setText("Replace this text with directory name (open folder in explorer, right click folder name in nav bar, hit 'copy dir'):  ");
-		textbox.setForeground(Color.WHITE);
+		textbox.setForeground(Color.lightGray);
+		textbox.setFont(new Font("Trebuchet MS", 12, 12));
 
 		//SET CONTENT PANE
 		Container c = getContentPane();
